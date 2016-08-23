ed7b08058a136a5c271f91aa8332eb957e5e84d8
everardo sanchez <everardo.ipn@gmail.com>
2016-08-23 17:35:52 -0500

    fix: location search engine

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -12,7 +12,7 @@ class LocationController < ApplicationController
 
     for i in(0..( histories.length-1) )
       distance = get_distance( user_info, histories[i] )
-      logger.info distance
+      logger.info "Distance to sounds #{distance}"
       if get_distance( user_info, histories[i] ) < 0.156
         near_histories.push(histories[i])
       end
@@ -23,17 +23,19 @@ class LocationController < ApplicationController
 
   def get_distance(u_location, a_location)
     #convert to floats
+    destiny = Hash.new
+    destiny[:latitude] = a_location[:latitude].to_f
+    destiny[:longitude] = a_location[:longitude].to_f
+
     u_location[:latitude] = u_location[:latitude].to_f
-    a_location[:latitude] = a_location[:latitude].to_f
     u_location[:longitude] = u_location[:longitude].to_f
-    a_location[:longitude] = a_location[:longitude].to_f
 
     # uses 's formula's formula
     earth_radius = 6371
-    d_lon = u_location[:longitude] - a_location[:longitude]
-    d_lat = u_location[:latitude] - a_location[:latitude]
+    d_lon = u_location[:longitude] - destiny[:longitude]
+    d_lat = u_location[:latitude] - destiny[:latitude]
 
-    a = ( (Math::sin(d_lat/2) ** 2) + Math::cos( a_location[:latitude]) * Math::cos( u_location[:latitude]) * Math::sin(d_lon/2) ) ** 2
+    a = ( (Math::sin(d_lat/2) ** 2) + Math::cos( destiny[:latitude]) * Math::cos( u_location[:latitude]) * Math::sin(d_lon/2) ) ** 2
     c = 2 * Math::atan2( Math::sqrt(a), Math::sqrt(1-a) )
     d = earth_radius * c
     return c
