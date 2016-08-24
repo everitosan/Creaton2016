0eb3147f797871ebef6a60dc9649bf20cbc1dcc9
everardo sanchez <everardo.ipn@gmail.com>
2016-08-24 12:25:30 -0500

    refact: record button methods

--- Creatn/app/src/main/java/rocks/evesan/creatnapp/ui/RecordButton.java
+++ Creatn/app/src/main/java/rocks/evesan/creatnapp/ui/RecordButton.java
@@ -1,6 +1,7 @@
 package rocks.evesan.creatnapp.ui;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.util.AttributeSet;
@@ -10,29 +11,48 @@ import android.widget.ToggleButton;
 
 import java.io.IOException;
 
+import rocks.evesan.creatnapp.R;
+
 /**
  * Created by evesan on 8/21/16.
  */
 public class RecordButton extends ImageButton {
 
     private MediaRecorder mRecorder = null;
+    private MediaPlayer button_sound = null;
+    private boolean sound_enabled = false;
     private String mFileName;
+    private Context context;
+    private boolean is_recording = false;
 
     public RecordButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        postConstructor(context);
     }
 
     public RecordButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        postConstructor(context);
     }
 
     public RecordButton(Context context) {
         super(context);
+        postConstructor(context);
+    }
+
+    public void postConstructor(Context ctx) {
+        this.context = ctx;
+        //Set audio for button
+        button_sound = MediaPlayer.create(this.context, R.raw.served);
+    }
+
+    public void enableSound() {
+        sound_enabled = true;
     }
 
 
     public void init() {
-
+        postConstructor(context);
         //Set file name and route
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/caminos_parlantes.3gp";
@@ -43,6 +63,8 @@ public class RecordButton extends ImageButton {
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mRecorder.setOutputFile(mFileName);
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
     }
 
     public String getFileName() {
@@ -64,12 +86,29 @@ public class RecordButton extends ImageButton {
     }
 
     public void stop() {
+        is_recording = false;
         mRecorder.stop();
     }
 
     public void release() {
         mRecorder.release();
         mRecorder = null;
+    }
+
+    public void startRecord() {
+        if(sound_enabled) {
+            button_sound.start();
+        }
+
+        if (is_recording) {
+            stop();
+            is_recording = false;
+        }
+
+        prepare();
+        start();
+        setImageDrawable(getResources().getDrawable(R.drawable.record_button_pressed));
+        is_recording = true;
     }
 
 }
