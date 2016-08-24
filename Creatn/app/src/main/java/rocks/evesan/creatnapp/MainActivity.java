08fa80e185954ab8819039b65aa4f15fd3cf3050
everardo sanchez <everardo.ipn@gmail.com>
2016-08-24 12:24:19 -0500

    refact: process of search and user journey

--- Creatn/app/src/main/java/rocks/evesan/creatnapp/MainActivity.java
+++ Creatn/app/src/main/java/rocks/evesan/creatnapp/MainActivity.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -32,50 +38,82 @@ public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private MediaPlayer notification;
-    private MediaPlayer push_button;
     private RecordButton mRecordButton;
-    private Location lastKnownLocation;
+    private Location currentLocation;
+    private String lProvider;
 
+    private ViewGroup rootScene;
+    private Scene ready_buttonScene, listen_recordedScene;
+    private TransitionManager mTransitionManager;
+
+    private String NOHISTORIES;
+
+
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        getSupportActionBar().hide();
         SnackBarError.init( findViewById(R.id.wrapper) );
 
+        setListeners();
+        setAnimations();
+        getStrings();
+
+        notification = MediaPlayer.create(this, R.raw.the_calling);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lProvider = LocationManager.GPS_PROVIDER;
+        getLastLocation();
+
+    }
+
+    private void setListeners() {
         mRecordButton = (RecordButton) findViewById(R.id.record);
         mRecordButton.setOnTouchListener(recordListener);
 
-        notification = MediaPlayer.create(this, R.raw.the_calling);
-        push_button = MediaPlayer.create(this, R.raw.served);
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        getLastLocation();
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipper);
+        //swipe action
+        swipeContainer.setOnRefreshListener( new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                searchHistories(currentLocation);
+            }
+        });
+    }
 
-        getSupportActionBar().hide();
+    private void getStrings() {
+        NOHISTORIES = getResources().getString(R.string.no_location);
+    }
 
+    private void setAnimations() {
+        rootScene = (ViewGroup) findViewById(R.id.wrapper);
+        ready_buttonScene = Scene.getSceneForLayout(rootScene, R.layout.ready_button, this);
+        listen_recordedScene = Scene.getSceneForLayout(rootScene, R.layout.listen_recorded, this);
+
+        mTransitionManager = TransitionInflater.from(this).inflateTransitionManager(R.transition.transition_manager, rootScene);
     }
 
     public View.OnTouchListener recordListener = new View.OnTouchListener() {
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+            mRecordButton.enableSound();
             switch (event.getAction()){
                 case MotionEvent.ACTION_DOWN:
-                    mRecordButton.prepare();
-                    mRecordButton.start();
-                    mRecordButton.setImageDrawable(getResources().getDrawable(R.drawable.mainbuttom_min));
-                    push_button.start();
+                    mRecordButton.startRecord();
                     break;
                 case MotionEvent.ACTION_UP:
                     mRecordButton.stop();
                     mRecordButton.release();
-                    Intent i = new Intent(MainActivity.this, UploadHistory.class);
-                    i.putExtra("latitude", Double.toString(lastKnownLocation.getLatitude()));
-                    i.putExtra("longitude", Double.toString(lastKnownLocation.getLongitude()));
-                    mRecordButton.setImageDrawable(getResources().getDrawable(R.drawable.mainbuttom));
-                    startActivity(i);
+                    //Intent i = new Intent(MainActivity.this, UploadHistory.class);
+                    //i.putExtra("latitude", Double.toString(currentLocation.getLatitude()));
+                    //i.putExtra("longitude", Double.toString(currentLocation.getLongitude()));
+                    mRecordButton.setImageDrawable(getResources().getDrawable(R.drawable.record_button));
+                    mTransitionManager.transitionTo(listen_recordedScene);
+                    //startActivity(i);
                     break;
 
             }
@@ -84,45 +122,86 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            searchHistories(location);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
+
     public void getLastLocation() {
         boolean permission = (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
         if (permission) {
-            String locationProvider = LocationManager.GPS_PROVIDER;
-
-            lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
-
-            searchData data = new searchData(Double.toString(lastKnownLocation.getLatitude()),  Double.toString(lastKnownLocation.getLongitude()) , "0");
-
-
-            Call <List<History>> call = apiAdapter.getApiService().seacrhHistories(data);
-            call.enqueue( new Callback<List<History>>() {
-
-                @Override
-                public void onResponse(Call<List<History>> call, Response<List<History>> response) {
-                    if( response.isSuccessful() ) {
-                        if (response.body().size() > 0) {
-                            notification.start();
-                            Intent i = new Intent(MainActivity.this, NearbyHistory.class);
-                            i.putExtra("list", (Serializable) response.body() );
-                            startActivity(i);
-                        } else {
-                            SnackBarError.getSnackBar("No hay historias en este lugar, sigue buscando ...").show();
-                        }
-                    } else {
-                        SnackBarError.getSnackBar("Some random error :C").show();
-                    }
-
-                }
-
-                @Override
-                public void onFailure(Call<List<History>> call, Throwable t) {
-                    SnackBarError.getSnackBar( getResources().getString(R.string.network_error) ).show();
-                }
-            } );
-
+            String locationProvider = lProvider;
+            Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            currentLocation = lastKnownLocation;
+            searchHistories(lastKnownLocation);
+            //enable repetitive scan of location
+            locationManager.requestLocationUpdates(lProvider, 2*60*1000, 200, locationListener);
         } else {
             SnackBarError.getSnackBar( getResources().getString(R.string.permission_denied) ).show();
         }
+    }
 
+    private void searchHistories(Location location) {
+        SnackBarError.getSnackBar(Double.toString(currentLocation.getLatitude()) + "," + Double.toString(currentLocation.getLongitude()) ).show();
+
+        searchData data = new searchData(Double.toString(location.getLatitude()),  Double.toString(location.getLongitude()) , "0");
+
+        Call <List<History>> call = apiAdapter.getApiService().seacrhHistories(data);
+        call.enqueue( new Callback<List<History>>() {
+
+            @Override
+            public void onResponse(Call<List<History>> call, Response<List<History>> response) {
+                swipeContainer.setRefreshing(false);
+                if( response.isSuccessful() ) {
+                    if (response.body().size() > 0) {
+                        notification.start();
+                        Intent i = new Intent(MainActivity.this, NearbyHistory.class);
+                        i.putExtra("list", (Serializable) response.body() );
+                        startActivity(i);
+                    } else {
+                        SnackBarError.getSnackBar(NOHISTORIES).show();
+                    }
+                } else {
+                    SnackBarError.getSnackBar("Some random error :C").show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<History>> call, Throwable t) {
+                swipeContainer.setRefreshing(false);
+                SnackBarError.getSnackBar( getResources().getString(R.string.network_error) ).show();
+            }
+        } );
+    }
+
+
+    public void playRecord(View v) {
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource(mRecordButton.getFileName());
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            Log.e("TAG", "prepare() failed");
+        }
+    }
+
+    public void deleteRecord(View v) {
+        mTransitionManager.transitionTo(ready_buttonScene);
+        setListeners();
+    }
+    @Override
+    public void onBackPressed() {
+        mTransitionManager.transitionTo(ready_buttonScene);
+        setListeners();
     }
 }
