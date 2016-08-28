384b362d4afd8f3586e3d25916cf512e076b15c3
everardo sanchez <everardo.ipn@gmail.com>
2016-08-28 13:25:34 -0500

    feat: creates tags if dont exist

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -2,6 +2,12 @@ class TagController < ApplicationController
 
   def index
     tags = Tag.all()
+    if tags.size == 0
+      t = Tag.create("name"=>"Mitos Urbanos")
+      t = Tag.create("name"=>"Amor")
+      t = Tag.create("name"=>"Crimen")
+      t = Tag.create("name"=>"Mitos Urbanos")
+      tags = Tag.all()
     jsonRespose(tags, 200)
   end
 end
