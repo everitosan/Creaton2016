2ad760d41e9e1367314618f13dec6d5bebb0192e
everardo sanchez <everardo.ipn@gmail.com>
2016-08-28 22:45:02 -0500

    fix: change to mp3

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -70,7 +70,7 @@ class HistoryController < ApplicationController
       end
       # copy file to s3
 
-      obj = S3_BUCKET.object(file_name + ".3gp")
+      obj = S3_BUCKET.object(file_name + ".mp3")
       obj.upload_file(file_path.to_s, acl:'public-read', content_type:'audio/mpeg')
 
       logger.info "#"*4
